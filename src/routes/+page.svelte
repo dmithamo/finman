456983abee196ce11ac9ab9476ffee4b8ec087a4
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-03-05 19:33:57 +0300

    [chore] clean up a little

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
 	import AppFooter from '$lib/components/app-footer.svelte';
 	import AppLogo from '$lib/components/app-logo.svelte';
 </script>
@@ -6,7 +6,7 @@
 <div class="flex h-full flex-col items-center justify-center gap-1 p-6">
 	<h1 class="text-2xl font-bold">Welcome to{' '}<AppLogo isNameOnly={true} />!</h1>
 	<p class="text-center text-sm">
-		Your privacy-preserving financial health companion (with AI&trade; and stuff)
+		Your privacy-preserving fin-health app (with AI&trade; and stuff!)
 	</p>
 </div>
 <AppFooter />
